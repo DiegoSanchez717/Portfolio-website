d09dd1a6a03fe5995e6d04aa8efc8af3b8959d4b
Diego Sanchez <diegoali_sanchez@hotmail.com>
2025-09-12 09:42:57 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let tabLinks = document.getElementsByClassName("tab-links");
+let tabContents = document.getElementsByClassName("tab-contents");
+function opentab(tabname) {
+  for(tabLink of tabLinks) {
+    tabLink.classList.remove("active-link");
+  }
+  for(tabContent of tabContents) {
+    tabContent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link")
+  document.getElementById(tabname).classList.add("active-tab");
+}
+
+let sidemenu = document.getElementById("sidemenu");
+
+function openMenu() {
+  sidemenu.style.right = "0";
+}
+function closeMenu() {
+  sidemenu.style.right = "-200px";
+}
